48b5895791e0f9fea477aff97fbf7ad4b9129067
Andrewjsy <kj10522002@gmail.com>
2023-07-30 13:56:34 +0900

    feat(photoset1)
    before core-js update to latest

--- wemeet-frontend/screens/Register/Additional/PhotoSetScreen.js
+++ wemeet-frontend/screens/Register/Additional/PhotoSetScreen.js
@@ -1,11 +1,22 @@
-import { SafeAreaView, Button, View, Text, Image, Alert } from "react-native";
+import {
+  SafeAreaView,
+  Button,
+  View,
+  Text,
+  Image,
+  Alert,
+  StyleSheet,
+  TouchableOpacity,
+} from "react-native";
 import React, { useState, useEffect } from "react";
 import * as ImagePicker from "expo-image-picker";
-import { CommonActions } from "@react-navigation/native";
+import { FontAwesome5 } from "@expo/vector-icons";
 import commonStyles from "../../../styles/commonStyles";
 import registerStyles from "../../../styles/registerStyles";
 import RegisterHeader from "../../../components/register/RegisterHeader";
 import RegisterCreditView from "../../../components/register/RegisterCreditView";
+import NextButton from "../../../components/NextButton";
+
 const instruction = "너의 사진을\n등록해줘";
 const PhotoSetScreen = ({ navigation }) => {
   const [profileImg, setProfileImg] = useState(null);
@@ -37,7 +48,7 @@ const PhotoSetScreen = ({ navigation }) => {
       //option finetune필요
       allowsEditing: true,
       quality: 1,
-      // aspect: [4, 3], ->
+      // aspect: [4, 3],
     });
     if (!result.canceled && type == 0) {
       setProfileImg(result.assets[0].uri);
@@ -50,8 +61,6 @@ const PhotoSetScreen = ({ navigation }) => {
   useEffect(() => {
     getPermission();
   }, []);
-  console.log("프로필", profileImg);
-  console.log("추가", subImg);
   return (
     <SafeAreaView style={commonStyles.safeAreaView}>
       <RegisterHeader navigation={navigation} back />
@@ -59,44 +68,98 @@ const PhotoSetScreen = ({ navigation }) => {
         <Text style={registerStyles.instText}>{instruction}</Text>
         <RegisterCreditView currentCredit={5} />
       </View>
-      <Text>
-        {
-          "다른 사람에게 보여지는 내 프로필 화면이에요\n서로 매칭되기 전까지는 사진 원본이 공개되지 않아요"
-        }
-      </Text>
-      {granted && <Text>'설정'의 'we-meet'에서 사진 권한을 설정해주세요.</Text>}
+      {/* {granted && <Text>'설정'의 'we-meet'에서 사진 권한을 설정해주세요.</Text>} */}
       <View style={{ flex: 1, alignItems: "center" }}>
         {/* 여기에 body내용 입력 */}
-
-        <View
+        <Text
           style={{
-            flexDirection: "row",
-            width: "100%",
-            justifyContent: "space-around",
+            alignSelf: "flex-start",
+            marginLeft: 20,
+            lineHeight: 20,
+            color: "gray",
           }}
         >
-          {profileImg && (
+          {
+            "다른 사람에게 보여지는 내 프로필 화면이에요\n서로 매칭되기 전까지는 사진 원본이 공개되지 않아요"
+          }
+        </Text>
+        <TouchableOpacity
+          style={{
+            backgroundColor: "white",
+            // borderWidth: 1,
+            marginTop: 10,
+            height: 400,
+            width: "85%",
+            borderRadius: 15,
+            justifyContent: "center",
+            alignItems: "center",
+            shadowColor: "#000",
+            shadowOffset: {
+              width: 0,
+              height: 2,
+            },
+            shadowOpacity: 0.25,
+            shadowRadius: 3.84,
+            elevation: 5,
+          }}
+          onPress={() => {
+            pickImageAsync(0);
+          }}
+        >
+          {profileImg ? (
+            //resizeMode 다시 보기, 누르고 있는 동안은 원본 사진 볼 수 있게! (pressable 활용!)
             <Image
               source={{ uri: profileImg }}
-              style={{ width: 150, height: 150 }}
-            ></Image>
+              style={{
+                height: "100%",
+                width: "100%",
+                borderRadius: 15,
+              }}
+              blurRadius={30}
+              resizeMode={"cover"}
+            />
+          ) : (
+            <>
+              <FontAwesome5 name="camera" size={40} color="gray" />
+              <Text
+                style={{
+                  color: "gray",
+                  fontSize: 20,
+                  letterSpacing: 2,
+                  textAlign: "center",
+                }}
+              >
+                1/2
+              </Text>
+              <Text
+                style={{
+                  width: "90%",
+                  color: "gray",
+                  position: "absolute",
+                  bottom: 30,
+                  textAlign: "center",
+                }}
+              >
+                대표 사진은 자신을 가장 잘 나타낼 수 있는 사진이야!{"\n"}
+                눈코입을 전부 확인할 수 있는 사진을 올려줘
+              </Text>
+            </>
           )}
-          {subImg && (
-            <Image
-              source={{ uri: subImg }}
-              style={{ width: 150, height: 150 }}
-            ></Image>
-          )}
-        </View>
+        </TouchableOpacity>
+        {subImg && (
+          <Image
+            source={{ uri: subImg }}
+            style={{ width: 150, height: 150 }}
+          ></Image>
+        )}
 
-        <Button
+        {/* <Button
           title="대표사진 업로드"
           onPress={() => {
             pickImageAsync(0);
           }}
           color={"pink"}
         />
-
         <Button
           theme="primary"
           title="추가사진 업로드"
@@ -104,31 +167,12 @@ const PhotoSetScreen = ({ navigation }) => {
             pickImageAsync(1);
           }}
           color={"pink"}
-        />
-        <Button
-          title={"사진 등록하기"}
-          onPress={() => {
-            navigation.navigate("AddInfoSet");
-            //사진 업로드 API주기
-          }}
-          color={"pink"}
-        ></Button>
-        <Button
-          title={"건너뛰기"}
-          onPress={() => {
-            //사진 업로드 API주기
-            navigation.dispatch(
-              CommonActions.reset({
-                index: 0,
-                routes: [{ name: "Main" }],
-              })
-            );
-          }}
-          color={"pink"}
-        ></Button>
+        /> */}
       </View>
+      <NextButton text={"사진 등록하기"} onPress={() => {}} />
     </SafeAreaView>
   );
 };
 
+const styles = StyleSheet.create({});
 export default PhotoSetScreen;
