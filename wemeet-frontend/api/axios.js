21d9baf016b92ca70f1cfca651d8316944413d02
Andrewjsy <kj10522002@gmail.com>
2023-11-08 13:47:53 +0900

    Update axios.js

--- wemeet-frontend/api/axios.js
+++ wemeet-frontend/api/axios.js
@@ -4,12 +4,6 @@ import { Alert } from "react-native";
 import { CommonActions } from "@react-navigation/native";
 import { BASE_URL, S3_PROFILE_BASE_URL } from "./baseUrl.js";
 
-// export const BASE_URL = "https://prod.wemeet.pics/v1/";
-// // "http://ec2-52-78-215-171.ap-northeast-2.compute.amazonaws.com:8080/v1/"; //for test only
-
-// export const S3_PROFILE_BASE_URL =
-//   "https://wemeet-profile-image.s3.ap-northeast-2.amazonaws.com/";
-
 const axiosDefault = axios.create({
   baseURL: BASE_URL,
 });
